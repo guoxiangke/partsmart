60fd6d2ce6609bc316c4282e3557ff11c6e34d06
guoxiangke <xiangkeguo@gmail.com>
2012-06-21 23:39:23 +0800

    dale0621

--- sites/all/themes/partsmart/nav.php
+++ sites/all/themes/partsmart/nav.php
@@ -21,15 +21,15 @@ print menu_tree($menu_name);
 	<div class="linkc">
 		
 		
-		<div class="linkcc floatr">
+		<div class="linkcc floatr" id="add_this">
 		<!-- AddThis Button BEGIN -->
-<div class="addthis_toolbox addthis_default_style ">
-<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-<a class="addthis_counter addthis_pill_style"></a>
-</div>
-<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
-<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4fc7b3ce3f96809f"></script>
-<!-- AddThis Button END -->
+			<div class="addthis_toolbox addthis_default_style ">
+				<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+				<a class="addthis_counter addthis_pill_style"></a>
+			</div>
+			<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+			<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4fc7b3ce3f96809f"></script>
+		<!-- AddThis Button END -->
 		</div>
 	</div>	<h1><span> <?php print $breadcrumb; ?></span></h1>
 
